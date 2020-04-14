a0307937ac175e87c9385aef59cbc78d2ab1e6d6
Umer Khan <umk499@mail.usask.ca>
2020-04-14 04:19:36 -0600

    Implemented board print.

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -10,26 +10,63 @@ public class GameOfLife {
         2. Any dead cell with three live neighbors becomes a live cell.
         3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
      */
-    protected String alive = "@";
-    protected String dead = ".";
-    protected String[][] grid;
+    protected char alive = '@';
+    protected char dead = '.';
+    protected char[][] grid;
 
     /**
      * Constructor
      * @param grid the initial grid
      */
-    public GameOfLife(String[][] grid){
+    public GameOfLife(char[][] grid){
         this.grid = grid;
     }
 
+    /**
+     * Getter for grid
+     */
+    public char[][] getGrid(){
+        return this.grid;
+    }
+
+    /**
+     * Updates the grid according to the 3 rules
+     */
+    public void update(){
+
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        char[][] initialGrid =
+                {
+                        {'@', '.', '.', '.', '.', '.', '.', '.'},
+                        {'@', '.', '@', '@', '.', '@', '.', '.'},
+                        {'@', '.', '.', '@', '.', '@', '@', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '@', '@', '@', '.', '.', '.'},
+                        {'.', '.', '@', '@', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '@', '.'},
+                        {'.', '.', '.', '.', '.', '.', '@', '.'}
+                };
+
+        GameOfLife Life = new GameOfLife(initialGrid);
+
         // Main loop
         while (true){
-
-
+            // Print the grid
+            // for each row:
+            for (int i = 0; i < initialGrid.length; i++){
+                // concatenate every character in the row and then print it all on one line
+                String row = "";
+                for (int j = 0; j < initialGrid[i].length; j++){
+                    row = row + initialGrid[i][j] + " ";
+                }
+                System.out.println(row);
+            }
 
             // Wait a second before updating board
             Thread.sleep(2000);
+            Life.update();
         }
     }
 }

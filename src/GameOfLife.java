f8f26c07cfa9c5f7b2c256905bbfbe2d22e72bd0
Umer Khan <umk499@mail.usask.ca>
2020-04-14 11:10:06 -0600

    Added a char to int converter and implemented top row of findNeighbours().

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -34,7 +34,79 @@ public class GameOfLife {
      */
     public void update(){
         for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                int[] neighbours = getNeighbours(i, j);
+            }
+        }
+    }
 
+    /**
+     * Returns an integer array consisting of 1's and 0's where
+     * 1 represents alive and 0 represents dead neighbours
+     * @param i index of row on the board
+     * @param j index of row item
+     */
+    public int[] getNeighbours(int i, int j){
+        // Initialize the array to size 8 since that is the max number of neighbours
+        int[] neighbours = new int[8];
+
+        // If the cell is in the top row
+        if (i == 0){
+            // If the cell is in the top left
+            if (j == 0){
+                neighbours[0] = convertToInt(board[i+1][j]);
+                neighbours[1] = convertToInt(board[i+1][j+1]);
+                neighbours[2] = convertToInt(board[i][j+1]);
+                return neighbours;
+            }
+            // If the cell is in the top right
+            else if (j == board[i].length-1){
+                neighbours[0] = convertToInt(board[i][j-1]);
+                neighbours[1] = convertToInt(board[i+1][j]);
+                neighbours[2] = convertToInt(board[i+1][j-1]);
+                return neighbours;
+            }
+            // If the cell is in the middle of the top row
+            else{
+                neighbours[0] = convertToInt(board[i][j-1]);
+                neighbours[1] = convertToInt(board[i][j+1]);
+                neighbours[2] = convertToInt(board[i+1][j-1]);
+                neighbours[3] = convertToInt(board[i+1][j]);
+                neighbours[4] = convertToInt(board[i+1][j+1]);
+                return neighbours;
+            }
+        }
+        // If the cell is in the bottom row
+        if (i == board.length - 1){
+            // If the cell is in the bottom left
+            if (j == 0){
+
+            }
+            // If the cell is in the bottom right
+            if (j == board[i].length - 1){
+
+            }
+            // If the cell is in the middle of the bottom row
+            else{
+
+            }
+        }
+    }
+
+    /**
+     * Converts a character from the board to either a 1 or a 0.
+     * 1 representing alive 0 representing dead
+     * @param c the character to convert
+     */
+    public int convertToInt(char c){
+        if (c == alive){
+            return 1;
+        }
+        else if (c == dead){
+            return 0;
+        }
+        else{
+            throw new RuntimeException("The given character (" + c + ") is unknown.");
         }
     }
 

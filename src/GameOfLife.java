907a13393c1277b9b8f0043222e79e46edbbb963
Umer Khan <umk499@mail.usask.ca>
2020-04-14 03:41:44 -0600

    Initial commit

---
+++ src/GameOfLife.java
@@ -0,0 +1,11 @@
+// Umer Khan 04/14/2020
+
+/**
+ * An implementation of Conway's Game of Life on the console using text
+ */
+public class GameOfLife {
+
+    public static void main(String[] args) {
+
+    }
+}

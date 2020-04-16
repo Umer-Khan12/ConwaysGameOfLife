164303c476f880eed7680b39bf23a2223ec3febe
Umer Khan <umk499@mail.usask.ca>
2020-04-15 19:24:54 -0600

    Changed getNeighbours() to return an int value and simplified the code to get neighbours. Removed sumArray() and charToInt() functions since the implementation no longer uses arrays for storing neighbours.

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -31,171 +31,65 @@ public class GameOfLife {
 
     /**
      * Updates the board according to the 3 rules
-     * analysis: Worst case time complexity is O(8n^2) = O(n^2).
      */
     public void update(){
+        // for each cell
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[i].length; j++){
-                // create an integer array of neighbours with either 0 or 1
-                int[] neighbours = getNeighbours(i, j);
-                // sum the array (This represents the number of alive neighbours)
-                int sum = arraySum(neighbours);
-                // Rule1:
-                if (board[i][j] == alive && (sum == 2 || sum == 3)){
-                    board[i][j] = alive;
-                }
-                // Rule2:
-                else if (board[i][j] == dead && sum == 3){
-                    board[i][j] = alive;
-                }
-                // Rule3:
-                else{
-                    board[i][j] = dead;
-                }
+                // number of alive neighbours
+                int aliveNeighbours = getNeighbours(i, j);
+                System.out.println("Sum for i=" + i + " and j=" + j + ": " + aliveNeighbours);
             }
         }
     }
 
     /**
-     * Sums the contents of an integer array and returns the answer as an integer
-     * @param arr the array to be summed
-     */
-    public int arraySum(int[] arr){
-        int sum = 0;
-        for (int i=0; i < arr.length; i++){
-            sum = sum + arr[i];
-        }
-        return sum;
-    }
-
-    /**
-     * Returns an integer array consisting of 1's and 0's where
-     * 1 represents alive and 0 represents dead neighbours
+     * Returns an integer representing the number of alive neighbours
+     * Analysis: O(8^2) = O(64) = O(1)
      * @param i index of row on the board
      * @param j index of row item
      */
-    public int[] getNeighbours(int i, int j){
-        // Initialize the array to size 8 since that is the max number of neighbours
-        int[] neighbours = new int[8];
-
-        // If the cell is in the top row
-        if (i == 0){
-            // If the cell is in the top left
-            if (j == 0){
-                neighbours[0] = convertToInt(board[i+1][j]);
-                neighbours[1] = convertToInt(board[i+1][j+1]);
-                neighbours[2] = convertToInt(board[i][j+1]);
-                return neighbours;
-            }
-            // If the cell is in the top right
-            else if (j == board[i].length-1){
-                neighbours[0] = convertToInt(board[i][j-1]);
-                neighbours[1] = convertToInt(board[i+1][j]);
-                neighbours[2] = convertToInt(board[i+1][j-1]);
-                return neighbours;
-            }
-            // If the cell is in the middle of the top row
-            else{
-                neighbours[0] = convertToInt(board[i][j-1]);
-                neighbours[1] = convertToInt(board[i][j+1]);
-                neighbours[2] = convertToInt(board[i+1][j-1]);
-                neighbours[3] = convertToInt(board[i+1][j]);
-                neighbours[4] = convertToInt(board[i+1][j+1]);
-                return neighbours;
+    public int getNeighbours(int i, int j){
+        int neighbours = 0;
+        char currentCell = board[i][j];
+        // Ignore the edges of the board and loop through the 3x3 grid within the board
+        // that corresponds to the cell and its neighbours.
+        if (i != 0 && i != board.length-1 && j != 0 && j != board[i].length-1){
+            for (int y = i-1; y < i+2; y++){
+                for (int x = j-1; x < j+2; x++){
+                    // if the neighbour is an alive cell then increase the number of neighbours
+                    if (board[y][x] == alive){
+                        neighbours++;
+                    }
+                }
             }
         }
-        // If the cell is in the bottom row
-        if (i == board.length - 1){
-            // If the cell is in the bottom left
-            if (j == 0){
-                neighbours[0] = convertToInt(board[i-1][j]);
-                neighbours[1] = convertToInt(board[i-1][j+1]);
-                neighbours[2] = convertToInt(board[i][j+1]);
-                return neighbours;
-            }
-            // If the cell is in the bottom right
-            else if (j == board[i].length - 1){
-                neighbours[0] = convertToInt(board[i][j-1]);
-                neighbours[1] = convertToInt(board[i-1][j]);
-                neighbours[2] = convertToInt(board[i-1][j-1]);
-                return neighbours;
-            }
-            // If the cell is in the middle of the bottom row
-            else{
-                neighbours[0] = convertToInt(board[i][j-1]);
-                neighbours[1] = convertToInt(board[i][j+1]);
-                neighbours[2] = convertToInt(board[i-1][j-1]);
-                neighbours[3] = convertToInt(board[i-1][j]);
-                neighbours[4] = convertToInt(board[i-1][j+1]);
-                return neighbours;
-            }
+        // If the cell we were checking for neighbours is alive then we counted an extra
+        // neighbour in the above loop so we should subtract 1
+        if (currentCell == alive){
+            neighbours -= 1;
         }
-        // If the cell is in the left column
-        // Ignore tl, tr, bl, br cases since those are covered above
-        if (j==0 && i != 0 && i != board.length - 1){
-            neighbours[0] = convertToInt(board[i-1][j]);
-            neighbours[1] = convertToInt(board[i-1][j+1]);
-            neighbours[2] = convertToInt(board[i][j+1]);
-            neighbours[3] = convertToInt(board[i+1][j+1]);
-            neighbours[4] = convertToInt(board[i+1][j]);
-            return neighbours;
-        }
-        // If the cell is in the right column
-        if (j == board[i].length-1 && i != 0 && i != board.length - 1){
-            neighbours[0] = convertToInt(board[i-1][j]);
-            neighbours[1] = convertToInt(board[i-1][j-1]);
-            neighbours[2] = convertToInt(board[i][j-1]);
-            neighbours[3] = convertToInt(board[i+1][j-1]);
-            neighbours[4] = convertToInt(board[i+1][j]);
-            return neighbours;
-        }
-        // If the cell has all 8 possible neighbours
-        else{
-            neighbours[0] = convertToInt(board[i-1][j-1]);
-            neighbours[1] = convertToInt(board[i-1][j]);
-            neighbours[2] = convertToInt(board[i-1][j+1]);
-            neighbours[3] = convertToInt(board[i][j-1]);
-            neighbours[4] = convertToInt(board[i][j+1]);
-            neighbours[5] = convertToInt(board[i+1][j-1]);
-            neighbours[6] = convertToInt(board[i+1][j]);
-            neighbours[7] = convertToInt(board[i+1][j+1]);
-            return neighbours;
-        }
-    }
-
-    /**
-     * Converts a character from the board to either a 1 or a 0.
-     * 1 representing alive 0 representing dead
-     * @param c the character to convert
-     */
-    public int convertToInt(char c){
-        if (c == alive){
-            return 1;
-        }
-        else if (c == dead){
-            return 0;
-        }
-        else{
-            throw new RuntimeException("The given character (" + c + ") is unknown.");
-        }
+        return neighbours;
     }
 
     public static void main(String[] args) throws InterruptedException {
         // Define the starting board
         char[][] initialBoard =
                 {
-                        {'@', '.', '.', '.', '.', '.', '.', '.'},
-                        {'@', '.', '@', '.', '@', '.', '.', '.'},
-                        {'.', '.', '.', '@', '@', '.', '.', '.'},
-                        {'.', '.', '.', '@', '.', '.', '.', '.'},
                         {'.', '.', '.', '.', '.', '.', '.', '.'},
-                        {'@', '@', '.', '.', '.', '.', '.', '.'},
-                        {'.', '@', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '@', '@', '@', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.'},
                         {'.', '.', '.', '.', '.', '.', '.', '.'}
                 };
 
         GameOfLife Life = new GameOfLife(initialBoard);
 
+        // Current generation
+        int gen = 1;
         // Main loop
         while (true){
             char[][] curBoard = Life.getBoard();
@@ -211,8 +105,9 @@ public class GameOfLife {
             }
 
             // Wait for some time before updating board
-            Thread.sleep(2000);
-            System.out.println("\n\n\n\n");
+            Thread.sleep(800);
+            System.out.println("\n\n\n\n\n\n\nGeneration " + gen);
+            gen++;
             Life.update();
         }
     }
